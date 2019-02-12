fd19f9c3de1d80b6bba2db491ff694fb857a75d4
fakemariastyles <mary.mirzayee@gmail.com>
2019-02-12 09:04:27 +0330

    a

---
+++ app/src/main/java/com/example/colors/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.colors
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+    val btn : Button = findViewById(R.id)
+    fun colors (id:String){
+
+    }
+}
